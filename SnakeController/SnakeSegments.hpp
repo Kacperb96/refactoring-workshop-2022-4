3059b3714927d17bc3ea0ebd642695ceb85c9794
Kacperb96 <kacperb1996@icloud.com>
2022-03-30 18:23:17 +0200

    Class updated with methods

--- SnakeController/SnakeSegments.hpp
+++ SnakeController/SnakeSegments.hpp
@@ -6,12 +6,21 @@
 
 #include "IEventHandler.hpp"
 #include "SnakeInterface.hpp"
+#include "SnakeController.hpp"
 
 class Event;
 class IPort;
 
+template <class T>
 class SnakeSegments{
 public:
+    bool isSegmentAtPosition(int x, int y) const;
+    T calculateNewHead() const;
+    void updateSegmentsIfSuccessfullMove(T const& newHead);
+    void addHeadSegment(T const& newHead);
+    void removeTailSegmentIfNotScored(T const& newHead);
+    void removeTailSegment();
 
 private:
+    std::list<T> m_segments;
 };
