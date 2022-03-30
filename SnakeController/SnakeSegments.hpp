efc2a19848c96c278c42bd7c8fba329adba6937f
Kacperb96 <kacperb1996@icloud.com>
2022-03-30 18:16:54 +0200

    SnakeSegment class added

---
+++ SnakeController/SnakeSegments.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <list>
+#include <memory>
+#include <stdexcept>
+#include <functional>
+
+#include "IEventHandler.hpp"
+#include "SnakeInterface.hpp"
+
+class Event;
+class IPort;
+
+class SnakeSegments{
+public:
+
+private:
+};
